a2a48a32593dc8e4d69a7b4dc59c54f8649b09c2
Sameera Wijesinghe <105413051+KMSWijesinghe@users.noreply.github.com>
2025-01-02 12:43:24 +0530

    25_02 Admin.jsx

--- client/src/Pages/Admin.jsx
+++ client/src/Pages/Admin.jsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import './Admin.css'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 
 
 function Admin()  {
+  const location = useLocation();
+    const studentDetails = location.state?.studentDetails;
     const [reviewers, setReviewers] = useState([]);
     const [selectedReviewer, setSelectedReviewer] = useState('');
   
@@ -18,30 +25,44 @@ function Admin()  {
   
       fetchReviewers();
     }, []);
+
+
+  const handleDownload = () => {
+    const localFilePath = `file:///C:/Users/Sameera%20Wijesinghe/Desktop/sample_project/server/${studentDetails.proposal_file_path}`;
+    window.open(localFilePath, "_blank");
+  };
   
     const handleReviewerChange = (event) => {
       setSelectedReviewer(event.target.value);
       // Optional: Additional logic when a reviewer is selected
     };
+
+    if (!studentDetails) {
+      return <div>No student details available</div>;
+    }
+
   return (
-    <div className='student'>
+  <div className='Admin'>
+    
         <div className='studentdetails'>
-            <h3>Name   : </h3>
-            <h3>Email  : </h3>
-            <h3>Course : </h3>
+            <h3>Name   : {studentDetails.name} </h3>
+            <h3>Email  : {studentDetails.email} </h3>
+            <h3>Tel    : {studentDetails.proposal_file_path}</h3>
         </div>
-         
+     
+        <div className='student'>
+
          <div>
-             <button className='bttn'>Download the proporsal</button>
+             <button className='bttn' onClick={handleDownload} >Download the proporsal</button>
         </div>
         
-        <div className="w-full max-w-xs">
+        <div className="sellection">
       <select 
         value={selectedReviewer}
         onChange={handleReviewerChange}
-        className="form-select block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        className="select_block"
       >
-        <option value="">Select a reviewer</option>
+        
         {reviewers.map((reviewer) => (
           <option 
             key={reviewer.id} 
@@ -54,10 +75,14 @@ function Admin()  {
     </div>
        
          <div>
-            <button className='bttn'>Upload the Reviewd Document</button>
+            <button className='bttn'>Upload the Reviewed Document</button>
 
         </div>
         
+    </div>
+
+    <Footer/>
+
     </div>
   )
 }
